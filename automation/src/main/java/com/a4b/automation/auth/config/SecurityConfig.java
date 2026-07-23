939bc01a38ead23f9fe260d4452b0c1b6d6ab8e1
abhaykumaryadav04 <abhaykumaryadavji9065@gmail.com>
2026-07-23 20:02:20 +0530

    small change in config

--- automation/src/main/java/com/a4b/automation/auth/config/SecurityConfig.java
+++ automation/src/main/java/com/a4b/automation/auth/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder() ;
     }
     @Bean
-    public  SecurityFilterChain securityFilterChain(HttpSecurity http){
+    public  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf-> csrf.disable())
             .authorizeHttpRequests(auth->auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
             .authenticationProvider(authProvider())
