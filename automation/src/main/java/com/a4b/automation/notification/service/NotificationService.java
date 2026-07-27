fc04d2ba4b1c325acacacb6b32328d560d77ae2c
abhaykumaryadav04 <abhaykumaryadavji9065@gmail.com>
2026-07-27 19:29:24 +0530

    Save notifications and send via WebSocket

--- automation/src/main/java/com/a4b/automation/notification/service/NotificationService.java
+++ automation/src/main/java/com/a4b/automation/notification/service/NotificationService.java
@@ -6,18 +6,22 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.a4b.automation.notification.dto.NotificationDto;
+import com.a4b.automation.notification.entity.Notification;
 import com.a4b.automation.notification.repo.NotificationRepo;
 import com.a4b.automation.user.entity.User;
 
 @Service
 public class NotificationService {
-  @Autowired
-  private NotificationRepo notificationRepo;
+ 
   @Autowired
   private SimpMessagingTemplate messagingTemplate;
+  @Autowired
+  private NotificationRepo notificationRepo;
     
 
   public void notifyUser(User reciever,NotificationDto dto){
-      messagingTemplate.convertAndSendToUser(reciever.getPhone(),"/queue/notifications",dto);    
+      messagingTemplate.convertAndSendToUser(reciever.getPhone(),"/queue/notifications",dto);
+      Notification notification=Notification.builder().message(dto.getMessage()).isRead(false).title(dto.getTitle()).reciver(reciever).build();    
+      notificationRepo.save(notification);
   }
 }
